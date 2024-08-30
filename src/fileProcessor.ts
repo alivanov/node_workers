33b0e6cea660b543f7b08377693a69a25f07adff
alivanov <ivanov@intspirit.com>
2024-08-30 14:03:10 +0300

    remove debug code

--- src/fileProcessor.ts
+++ src/fileProcessor.ts
@@ -1,26 +1,3 @@
-/* const fs = require('fs');
-
-function processFile(filePath) {
-  return new Promise((resolve, reject) => {
-    console.log(`Processing file: ${filePath}`);
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) return reject(err);
-
-      const words = data
-        .replace(/[^\w\s]/g, '')
-        .toLowerCase()
-        .split(/\s+/);
-
-      console.log(`File ${filePath} contains ${words.length} words`);
-      resolve(words);
-    });
-  });
-}
-
-module.exports = {
-  processFile,
-}; */
-
 import fs from 'fs';
 import { promisify } from 'util';
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,3 @@
-/* const fs = require('fs');
-const path = require('path');
-
-function readFilesFromDirectory(directory) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(directory, (err, files) => {
-      if (err) return reject(err);
-      const textFiles = files.filter(file => path.extname(file) === '.txt');
-      resolve(textFiles.map(file => path.join(directory, file)));
-    });
-  });
-}
-
-module.exports = {
-  readFilesFromDirectory,
-}; */
-
 import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
